7da67632ac33d41ec49223ead527fe08959f1731
Oscar A Castro <oacastro2@gmail.com>
2020-08-27 15:23:25 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,12 @@
-
-def find_max_value( [1, 2, 1, 1, 3, 4, 3, 5, 4, 3, 2, 1] )
+find_max_value( [1, 2, 1, 1, 3, 4, 3, 5, 4, 3, 2, 1] )
+def find_max_value(array)
   count = 0 
   max_value = -1 
   while count < array.length do 
-    if max_value < array[count]
+      if max_value < array[count]
       max_value = array[count]
-    end 
+      end 
         count += 1 
-    end     
+    end 
+    max_value 
 end
